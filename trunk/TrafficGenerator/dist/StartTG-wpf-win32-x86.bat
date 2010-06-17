c38d54be8c6d120b4748fac350a2887ec18c9389
alexandru.cibotari <alexandru.cibotari@438b6f2c-073c-4ae8-a17e-f6918783e638>
2010-06-17 08:30:46 +0000

    Add Build file for win32-x86_64

---
+++ trunk/TrafficGenerator/dist/StartTG-wpf-win32-x86.bat
@@ -0,0 +1,25 @@
+@echo off
+title TestNetworkTool Console
+:start
+echo Starting TestNetworkTool.
+echo.
+REM -------------------------------------
+REM Default parameters for a basic server.
+java -jar tg-wpf-win32-x86.jar
+REM
+REM -------------------------------------
+
+SET CLASSPATH=%OLDCLASSPATH%
+
+
+if ERRORLEVEL 1 goto error
+goto end
+:error
+echo.
+echo TestNetworkTool Terminated Abnormaly, Please Verify Your Files.
+echo.
+:end
+echo.
+echo TestNetworkTool Terminated.
+echo.
+pause
