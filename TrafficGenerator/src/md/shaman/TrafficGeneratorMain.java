9e685f05f90180f9ed04dfbd1447031865720bb1
alexandru.cibotari <alexandru.cibotari@438b6f2c-073c-4ae8-a17e-f6918783e638>
2010-10-11 09:43:14 +0000

    Table is not Editable

--- TrafficGenerator/src/md/shaman/TrafficGeneratorMain.java
+++ TrafficGenerator/src/md/shaman/TrafficGeneratorMain.java
@@ -270,9 +270,16 @@ public class TrafficGeneratorMain extends FrameView {
             Class[] types = new Class [] {
                 java.lang.Long.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false, true
+            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         processTable.setComponentPopupMenu(processPopupMenu);
@@ -490,7 +497,6 @@ public class TrafficGeneratorMain extends FrameView {
         stopMenuItem.setAction(actionMap.get("Stop")); // NOI18N
         stopMenuItem.setIcon(resourceMap.getIcon("stopMenuItem.icon")); // NOI18N
         stopMenuItem.setText(resourceMap.getString("stopMenuItem.text")); // NOI18N
-        stopMenuItem.setEnabled(false);
         stopMenuItem.setName("stopMenuItem"); // NOI18N
         processPopupMenu.add(stopMenuItem);
 
